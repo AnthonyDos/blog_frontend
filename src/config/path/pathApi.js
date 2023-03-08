dc536001bcf855416a8d1ebdcd77f75e8828c7ad
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2023-03-08 19:08:54 +0100

    corrections des routes api

---
+++ src/config/path/pathApi.js
@@ -0,0 +1,20 @@
+export const BASE_URL = "http://localhost:3000/api";
+
+//path auth
+export const SIGNUP = "/signup";
+export const LOGIN = "/login";
+
+//path users
+export const USERS = "/auth/user/";
+export const USER_ID = "/auth/user/";
+export const UPDATE_USER = "/auth/user/update/";
+export const DELETE_USER = "/auth/user/delete/";
+
+//path articles
+export const ARTICLES = "/article/";
+export const ARTICLE_ID = "/article/";
+export const UPDATE_ARTICLE = "/article/update/";
+export const DELETE_ARTICLE = "/article/delete/";
+
+//path comments
+export const ADD_COMMENT = "/article/:_id/comment/";
